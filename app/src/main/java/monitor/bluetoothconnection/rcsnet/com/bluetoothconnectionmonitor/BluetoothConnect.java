8397220c970d5b17db1a802e2fa6d5693fa9a81c
Rémi Cohen-Scali <rcoscali@gmail.com>
2016-05-13 09:17:33 +0200

    Fixed media player / resource problem.

--- app/src/main/java/monitor/bluetoothconnection/rcsnet/com/bluetoothconnectionmonitor/BluetoothConnect.java
+++ app/src/main/java/monitor/bluetoothconnection/rcsnet/com/bluetoothconnectionmonitor/BluetoothConnect.java
@@ -1,5 +1,6 @@
 package monitor.bluetoothconnection.rcsnet.com.bluetoothconnectionmonitor;
 
+import android.bluetooth.BluetoothAdapter;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,8 +19,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class BluetoothConnect extends AppCompatActivity {
+public class BluetoothConnect extends AppCompatActivity
+{
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -36,7 +40,8 @@ public class BluetoothConnect extends AppCompatActivity {
     private ViewPager mViewPager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_connect);
 
@@ -50,35 +55,40 @@ public class BluetoothConnect extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+        fab.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View view)
+                    {
+                        Snackbar.make(view,
+                                      "Replace with your own action",
+                                      Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                    }
+                }
+        );
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_bluetooth_connect, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
@@ -88,21 +98,24 @@ public class BluetoothConnect extends AppCompatActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment
+    {
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
+        public PlaceholderFragment()
+        {
         }
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
+        public static PlaceholderFragment newInstance(int sectionNumber)
+        {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
@@ -111,12 +124,55 @@ public class BluetoothConnect extends AppCompatActivity {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater,
+                                 ViewGroup container,
+                                 Bundle savedInstanceState)
+        {
+            Log.v("BluetoothMonitor", "Fragment - onCreateView" + getString(R.string.section_format,
+                    getArguments().getInt(ARG_SECTION_NUMBER)));
             View rootView = inflater.inflate(R.layout.fragment_bluetooth_connect, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            textView.setText(getString(R.string.section_format,
+                                       getArguments().getInt(ARG_SECTION_NUMBER)));
+            switch (getArguments().getInt(ARG_SECTION_NUMBER))
+            {
+                case 0:
+                    doStep1();
+                    break;
+                case 1:
+                    doStep2();
+                    break;
+                case 2:
+                    doStep3();
+                    break;
+            }
             return rootView;
+        }
+
+        /*
+         * Device Connection Setup: First Step
+         */
+        private void doStep1()
+        {
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (bluetoothAdapter == null)
+                Toast.makeText(getContext(), "Bluetooth not available", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(getContext(), "Bluetooth available", Toast.LENGTH_SHORT).show();
+        }
+
+        /*
+         * Device Connection Setup: Second Step
+         */
+        private void doStep2()
+        {
+        }
+
+        /*
+         * Device Connection Setup: Third Step
+         */
+        private void doStep3()
+        {
         }
     }
 
@@ -124,34 +180,40 @@ public class BluetoothConnect extends AppCompatActivity {
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    public class SectionsPagerAdapter extends FragmentPagerAdapter
+    {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
+        public SectionsPagerAdapter(FragmentManager fm)
+        {
             super(fm);
         }
 
         @Override
-        public Fragment getItem(int position) {
+        public Fragment getItem(int position)
+        {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
             return PlaceholderFragment.newInstance(position + 1);
         }
 
         @Override
-        public int getCount() {
+        public int getCount()
+        {
             // Show 3 total pages.
             return 3;
         }
 
         @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
+        public CharSequence getPageTitle(int position)
+        {
+            switch (position)
+            {
                 case 0:
-                    return "SECTION 1";
+                    return "Check Bluetooth Device";
                 case 1:
-                    return "SECTION 2";
+                    return "List Reachable Devices";
                 case 2:
-                    return "SECTION 3";
+                    return "Connect And Monitor";
             }
             return null;
         }
