725fa967d01abaec1184dcffa767b881a4875e5c
Rémi Cohen-Scali <remi.cohen-scali@laposte.net>
2016-05-15 11:29:08 +0200

    Managing bluetooth activation correctly

--- app/src/main/java/monitor/bluetoothconnection/rcsnet/com/bluetoothconnectionmonitor/BluetoothConnect.java
+++ app/src/main/java/monitor/bluetoothconnection/rcsnet/com/bluetoothconnectionmonitor/BluetoothConnect.java
@@ -1,5 +1,7 @@
 package monitor.bluetoothconnection.rcsnet.com.bluetoothconnectionmonitor;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +14,11 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.*;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BluetoothConnect extends AppCompatActivity
 {
+    private static final int REQUEST_CODE_ENABLE_BLUETOOTH = 0;
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -63,6 +67,15 @@ public class BluetoothConnect extends AppCompatActivity
     }
 
     @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != REQUEST_CODE_ENABLE_BLUETOOTH)
+            return;
+        if (resultCode != RESULT_OK)
+            finish();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -102,6 +115,7 @@ public class BluetoothConnect extends AppCompatActivity
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
+        private static final int REQUEST_CODE_ENABLE_BLUETOOTH = 0;
 
         private int mSectionNumber;
 
@@ -130,11 +144,36 @@ public class BluetoothConnect extends AppCompatActivity
             mSectionNumber = getArguments().getInt(ARG_SECTION_NUMBER);
             Log.v("BluetoothMonitor",
                   "Fragment - onCreateView: " + getString(R.string.section_format, mSectionNumber));
-            View rootView = inflater.inflate(R.layout.fragment_bluetooth_connect, container, false);
+            View             rootView         = inflater.inflate(R.layout.fragment_bluetooth_connect, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format,
-                                       mSectionNumber));
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter ();
+            if (bluetoothAdapter == null)
+            {
+                Toast.makeText (this.getActivity ().getApplicationContext (), "Pas de Bluetooth",
+                                Toast.LENGTH_SHORT
+                               ).show ();
+                textView.setText("No bluetooth device");
+            }
+            else
+            {
+                textView.setText(getString(R.string.section_format,
+                                           mSectionNumber));
+                if (!bluetoothAdapter.isEnabled()) {
+                    Intent enableBlueTooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBlueTooth, REQUEST_CODE_ENABLE_BLUETOOTH);
+                }
+            }
+
             return rootView;
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode != REQUEST_CODE_ENABLE_BLUETOOTH)
+                return;
+            if (resultCode == 0)
+                this.getActivity ().finish ();
         }
 
         @Override
@@ -158,10 +197,15 @@ public class BluetoothConnect extends AppCompatActivity
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter
     {
+        private final int STATE_START = 1;
+        private final int STATE_DEVICE_SELECTED = 2;
+        private final int STATE_DEvICE_CONNECTED = 3;
+        private int mState;
 
         public SectionsPagerAdapter(FragmentManager fm)
         {
             super(fm);
+            mState = STATE_START;
         }
 
         @Override
@@ -176,7 +220,17 @@ public class BluetoothConnect extends AppCompatActivity
         public int getCount()
         {
             // Show 3 total pages.
-            return 1;
+            switch (mState)
+            {
+            case STATE_START:
+                return 1;
+            case STATE_DEVICE_SELECTED:
+                return 2;
+            case STATE_DEvICE_CONNECTED:
+                return 3;
+            default:
+                return 1;
+            }
         }
 
         @Override
