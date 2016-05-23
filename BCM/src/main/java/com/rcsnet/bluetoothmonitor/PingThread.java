0d0741988c93fb04de72450e35aef6704c80e796
Rémi Cohen-Scali <remi.cohen-scali@laposte.net>
2016-05-23 10:34:42 +0200

    Factorization of sendMessage, sendError, sendTransition & sendInOutData in connectionManagement superclass

--- BCM/src/main/java/com/rcsnet/bluetoothmonitor/PingThread.java
+++ BCM/src/main/java/com/rcsnet/bluetoothmonitor/PingThread.java
@@ -1,8 +1,6 @@
 package com.rcsnet.bluetoothmonitor;
 
 import android.bluetooth.BluetoothSocket;
-import android.os.Bundle;
-import android.os.Message;
 
 import java.util.Arrays;
 
@@ -80,19 +78,10 @@ public class PingThread
             }
             mmConnectedThread.write(buffer);
 
-            Message msg  = mHandler.obtainMessage(BluetoothClientServer.MESSAGE_STATE_TRANSITION);
-            Bundle data = new Bundle();
-            msg.arg1 = mActivity.getState();
-            msg.arg2 = mActivity.setState(false);
-            data.putString("reason", mResources.getString(R.string.ping_thread_send_request));
-            msg.setData(data);
+            sendTransition(R.string.ping_thread_send_request, false);
 
             // Tell main UI thread about data we sent in ping request
-            msg = mHandler.obtainMessage(BluetoothClientServer.MESSAGE_DATAOUT);
-            data = new Bundle();
-            msg.arg1 = bytes;
-            data.putString("dataout", new String(Arrays.copyOfRange(buffer, 1, buffer.length)));
-            msg.setData(data);
+            sendDataInOutMessage(false, Arrays.copyOfRange(buffer, 1, buffer.length), bytes);
 
             try
             {
@@ -101,6 +90,7 @@ public class PingThread
             catch (InterruptedException ignored)
             {
             }
+
             mmTimeoutThread.cancel();
             try
             {
@@ -121,13 +111,9 @@ public class PingThread
             {
             }
 
-            if (mActivity.getState() == BluetoothClientServer.PING_STATE_ALARM) {
-                msg = mHandler.obtainMessage(BluetoothClientServer.MESSAGE_STATE_TRANSITION);
-                data = new Bundle();
-                msg.arg1 = mActivity.getState();
-                msg.arg2 = mActivity.setState(false);
-                data.putString("reason", mResources.getString(R.string.alarm_launched));
-                msg.setData(data);
+            if (mActivity.getState() == BluetoothClientServer.PING_STATE_ALARM)
+            {
+                sendTransition(R.string.alarm_launched, false);
                 mNotEnd = false;
             }
         }
